7a102563c677296ef032be49f417440a9888fed1
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-19 18:40:04 +0200

    Planteamiento inicial ejercicio para Servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,47 +9,20 @@ import { MiServicioService } from './services/mi-servicio.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  title = 'Hola mundo';
+export class AppComponent {
 
-  constructor(private miServicio: MiServicioService) {}
+  constructor() {
 
-  async ngOnInit() {
+    // 1. Llamar al método getDirectUsers
 
-    this.miServicio.getRecetas().subscribe({
-      next: (recetas: any) => console.log(recetas),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('La petición de recetas se ha completado'),
-    });
 
-    this.miServicio.getFotos().subscribe({
-      next: (fotos: any) => console.log(fotos),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('La petición de fotos se ha completado'),
-    });
+    // 2. Llamar al método getUsersByPromise
 
-    this.miServicio.getFoto().subscribe({
-      next: (fotos: any) => console.log(fotos),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('La petición de foto se ha completado'),
-    });
 
-    try {
-      const numero = await this.miServicio.getPromesa();
-      console.log('El número es menor que 5: ' + numero);
-    } catch (error) {
-      console.log(error);
-    }
-
-    try {
-      const productos = await this.miServicio.getProducts();
-      console.log(productos);
-    } catch (error) {
-      alert('Se ha producido un error al obtener los prodcutos');
-    }
-
+    // 3. Llamar al método getUsersByObservable
 
 
   }
+
 
 }
