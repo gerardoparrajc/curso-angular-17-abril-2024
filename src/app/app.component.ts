8ea34fe096fc8190599111b1f2ce2cb94b3b42cf
Gerardo Parra <gerardoparrajuandelacruz@gmail.com>
2024-04-26 17:57:30 +0200

    Creada prueba para ver si se lanza un action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { getItems } from './state/actions/items.actions';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+      this.store.dispatch(getItems());
+  }
+
 }
